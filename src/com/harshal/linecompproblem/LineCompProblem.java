97c5b3398be5d37608bd44b6bd115e3d8c875f25
harshaltamboli <harshaltamboli429@gmail.com>
2024-03-04 14:55:23 +0530

    [ADDED]:harshal LineCompProblem in jva

---
+++ src/com/harshal/linecompproblem/LineCompProblem.java
@@ -0,0 +1,15 @@
+package com.harshal.linecompproblem;
+
+public class LineCompProblem {
+	
+	
+
+	public static void main(String[] args){
+		
+		
+		System.out.println("Welcome To Line Computation Program");
+
+	}
+
+	
+}
