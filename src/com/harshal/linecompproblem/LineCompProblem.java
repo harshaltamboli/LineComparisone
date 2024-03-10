5d0e18aef71fbaf0fac57711d58269421dc47d18
harshaltamboli <harshaltamboli429@gmail.com>
2024-03-11 03:50:29 +0530

    [MODIFIED]: harshal - Refactered this code to check equality of two line

--- src/com/harshal/linecompproblem/LineCompProblem.java
+++ src/com/harshal/linecompproblem/LineCompProblem.java
@@ -1,36 +1,56 @@
+
 package com.harshal.linecompproblem;
 
 import java.util.Scanner;
 
 public class LineCompProblem {
+		
+	int x1;
+	int x2;
+	int y1;
+	int y2;
+	String line;
 	
 	
-
-	public static void main(String[] args){
+	public void lineInput() {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter the X1,Y1 and X2,Y2 points for the Line:");
 		
-		int x1;
-		int x2;
-		int y1 = 0;
-		int y2;
-		double Lenght;  
-		
-		Scanner scanner =new Scanner(System.in);
-		
-
-		System.out.println("Welcome To Line Computation Program");
-		
-		System.out.println("Enter The x1,y1 and x2,y2 point for the line");
-		
-		
-		x1= scanner.nextInt();
-		x2= scanner.nextInt();
-		y1= scanner.nextInt();
-		y2= scanner.nextInt();
-		
-		Lenght= Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
-
-		System.out.println("Lenght of line"+Lenght);
+		x1 = scanner.nextInt();
+		y1 = scanner.nextInt();
+		x2 = scanner.nextInt();
+		y2 = scanner.nextInt();
 	}
-
 	
+	public double lineCalculate() {
+		
+			return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));	
+	}
+	
+	public void lineCheck(LineCompProblem linecompproblem2)
+
+	{
+		if(this.lineCalculate() == linecompproblem2.lineCalculate()) {
+		
+			System.out.println("Both Line are Same");
+		}
+		
+		else {
+			
+			System.out.println("Line are Not Same");
+		}	
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to Line Comparison Computation Program ");
+		
+		LineCompProblem linecompproblem1 = new LineCompProblem();
+		LineCompProblem linecompproblem2 = new LineCompProblem();
+		
+		linecompproblem1.lineInput();
+		linecompproblem2.lineInput();
+	
+		linecompproblem1.lineCheck(linecompproblem2);
+	}
 }
